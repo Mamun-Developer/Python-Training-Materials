13313041e5c766df967683f98caca9775612b4c2
mdmamuncse <mdmamuncse110@gmail.com>
2021-04-19 02:18:57 +0600

    test method added

--- Class5/gun.py
+++ Class5/gun.py
@@ -6,9 +6,15 @@
 # 5. Auto reload support = True
 # 6. Bullet size = 9mm
 import time
+
+
+def test():
+    pass
+
+
 class gun:
-    def __init__(self,magazineLimit = 6,perTrigerOutput = 1, reloadTime = 3,
-                 autoReload = True,bulletSize = "9mm",bulletsInTheBox=20):
+    def __init__(self, magazineLimit=6, perTrigerOutput=1, reloadTime=3,
+                 autoReload=True, bulletSize="9mm", bulletsInTheBox=20):
         self.magazineLimit = magazineLimit
         self.perTrigerOutput = perTrigerOutput
         self.reloadTime = reloadTime
@@ -19,7 +25,7 @@ class gun:
         self.stopFiring = False
 
     def fire(self):
-        if self.getRemainingBulletsInGun()==0:
+        if self.getRemainingBulletsInGun() == 0:
             print("Magazine Empty")
             if self.autoReload == True:
                 print("Auto Reloading")
@@ -28,33 +34,32 @@ class gun:
                 PROCEED_RELOAD = 1
                 DONT_RELOAD = 2
                 command = int(input("1. Reload\n2. Dont reload\n"))
-                if command==PROCEED_RELOAD:
+                if command == PROCEED_RELOAD:
                     print("Reloading")
                     self.reload()
-                elif command==DONT_RELOAD:
+                elif command == DONT_RELOAD:
                     print("Reload Required")
 
-        if self.getRemainingBulletsInGun()>0:
+        if self.getRemainingBulletsInGun() > 0:
             self.loadedBulletsCount = self.loadedBulletsCount - 1
             print(f"Fire: {self.magazineLimit}/{self.loadedBulletsCount}")
 
     def reload(self):
-        print("Wait for "+str(self.reloadTime)+"secs")
+        print("Wait for " + str(self.reloadTime) + "secs")
         time.sleep(self.reloadTime)
         bulletToReload = self.magazineLimit - self.getRemainingBulletsInGun()
-        if self.getRemainingBulletsInBox()>=bulletToReload:
+        if self.getRemainingBulletsInBox() >= bulletToReload:
             self.loadedBulletsCount = self.loadedBulletsCount + bulletToReload
             self.bulletsInTheBox = self.bulletsInTheBox - bulletToReload
             print("reloaded: Magazine Full")
-        elif self.getRemainingBulletsInBox()==0:
+        elif self.getRemainingBulletsInBox() == 0:
             print("Not enough bullets in the box")
             self.stopFiring = True
 
-        elif self.getRemainingBulletsInBox()<bulletToReload:
+        elif self.getRemainingBulletsInBox() < bulletToReload:
             self.loadedBulletsCount = self.loadedBulletsCount + self.getRemainingBulletsInBox()
             self.bulletsInTheBox = 0
-            print("reloaded: Magazine - "+str(self.loadedBulletsCount))
-
+            print("reloaded: Magazine - " + str(self.loadedBulletsCount))
 
     def clearMagazine(self):
         temp = self.loadedBulletsCount
@@ -66,6 +71,7 @@ class gun:
 
     def getRemainingBulletsInBox(self):
         return self.bulletsInTheBox
+
 
 shortGun = gun()
 # Gun Commands
@@ -79,7 +85,8 @@ REMAINING_BULLETS_IN_BOX = 5
 STOP_FIRING = 6
 
 while shortGun.stopFiring == False:
-    print("COMMAND LIST:\n1. Fire \n2. Reload \n3. Clear Magazine  \n4. Loaded Bullets \n5. Bullets in the box \n6. Stop Firing")
+    print(
+        "COMMAND LIST:\n1. Fire \n2. Reload \n3. Clear Magazine  \n4. Loaded Bullets \n5. Bullets in the box \n6. Stop Firing")
     command = int(input("COMMAND NO: "))
 
     if command == FIRE:
@@ -90,9 +97,9 @@ while shortGun.stopFiring == False:
         shortGun.clearMagazine()
         print("Magazine Cleared")
     elif command == LOADED_BULLETS:
-        print("Bullets in the MAGAZINE: "+str(shortGun.getRemainingBulletsInGun()))
+        print("Bullets in the MAGAZINE: " + str(shortGun.getRemainingBulletsInGun()))
     elif command == REMAINING_BULLETS_IN_BOX:
-        print("Bullets in the BOX: "+str(shortGun.getRemainingBulletsInBox()))
+        print("Bullets in the BOX: " + str(shortGun.getRemainingBulletsInBox()))
     elif command == STOP_FIRING:
         shortGun.stopFiring = True
         print("Firing stopped")
